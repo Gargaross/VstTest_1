3c7938b819baac7050f6d79b3828f77bbefa2b40
Gargaross <kallenback@gmail.com>
2018-01-01 22:53:40 +0100

    plugin now initializes

--- TheBypasser/TheBypasser/BypassProcessor.cpp
+++ TheBypasser/TheBypasser/BypassProcessor.cpp
@@ -5,43 +5,193 @@ namespace Steinberg {
 namespace Vst {
 	BypassProcessor::BypassProcessor()
 	{
-
+		//setControllerClass(ADelayControllerUID);
 	}
 
 
-	tresult BypassProcessor::initialize(FUnknown* context)
+	tresult PLUGIN_API BypassProcessor::initialize(FUnknown* context)
 	{
-		return 0;
+		tresult result = AudioEffect::initialize(context);
+		if (result == kResultTrue)
+		{
+			addAudioInput(STR16("AudioInput"), SpeakerArr::kStereo);
+			addAudioOutput(STR16("AudioOutput"), SpeakerArr::kStereo);
+		}
+		return result;
 	}
 
 
-	tresult BypassProcessor::setBusArrangements(SpeakerArrangement* inputs, int32 numIns, SpeakerArrangement* outputs, int32 numOuts)
+	tresult PLUGIN_API BypassProcessor::setBusArrangements(SpeakerArrangement* inputs, int32 numIns, SpeakerArrangement* outputs, int32 numOuts)
 	{
-		return 0;
+		// we only support one in and output bus and these buses must have the same number of channels
+		if (numIns == 1 && numOuts == 1 && inputs[0] == outputs[0])
+			return AudioEffect::setBusArrangements(inputs, numIns, outputs, numOuts);
+		return kResultFalse;
 	}
 
 
-	tresult BypassProcessor::setActive(TBool state)
+	tresult PLUGIN_API BypassProcessor::setActive(TBool state)
 	{
-		return 0;
+		/*
+		SpeakerArrangement arr;
+		if (getBusArrangement(kOutput, 0, arr) != kResultTrue)
+			return kResultFalse;
+		int32 numChannels = SpeakerArr::getChannelCount(arr);
+		if (numChannels == 0)
+			return kResultFalse;
+
+		if (state)
+		{
+			mBuffer = (float**)std::malloc(numChannels * sizeof(float*));
+
+			size_t size = (size_t)(processSetup.sampleRate * sizeof(float) + 0.5);
+			for (int32 channel = 0; channel < numChannels; channel++)
+			{
+				mBuffer[channel] = (float*)std::malloc(size);	// 1 second delay max
+				memset(mBuffer[channel], 0, size);
+			}
+			mBufferPos = 0;
+		}
+		else
+		{
+			if (mBuffer)
+			{
+				for (int32 channel = 0; channel < numChannels; channel++)
+				{
+					std::free(mBuffer[channel]);
+				}
+				std::free(mBuffer);
+				mBuffer = 0;
+			}
+		}
+		return AudioEffect::setActive(state);
+		*/
+		return kResultTrue;
 	}
 
 
-	tresult BypassProcessor::process(ProcessData& data)
+	tresult PLUGIN_API BypassProcessor::process(ProcessData& data)
 	{
-		return 0;
+		/*
+		if (data.inputParameterChanges)
+		{
+			int32 numParamsChanged = data.inputParameterChanges->getParameterCount();
+			for (int32 index = 0; index < numParamsChanged; index++)
+			{
+				IParamValueQueue* paramQueue = data.inputParameterChanges->getParameterData(index);
+				if (paramQueue)
+				{
+					ParamValue value;
+					int32 sampleOffset;
+					int32 numPoints = paramQueue->getPointCount();
+					switch (paramQueue->getParameterId())
+					{
+					case kDelayId:
+						if (paramQueue->getPoint(numPoints - 1, sampleOffset, value) == kResultTrue)
+							mDelay = value;
+						break;
+					case kBypassId:
+						if (paramQueue->getPoint(numPoints - 1, sampleOffset, value) == kResultTrue)
+						{
+							mBypass = (value > 0.5f);
+						}
+						break;
+					}
+				}
+			}
+		}
+
+		if (data.numSamples > 0)
+		{
+			SpeakerArrangement arr;
+			getBusArrangement(kOutput, 0, arr);
+			int32 numChannels = SpeakerArr::getChannelCount(arr);
+
+			// TODO do something in Bypass : copy inpuit to output if necessary...
+
+			// apply delay
+			int32 delayInSamples = std::max<int32>(1, (int32)(mDelay * processSetup.sampleRate)); // we have a minimum of 1 sample delay here
+			for (int32 channel = 0; channel < numChannels; channel++)
+			{
+				float* inputChannel = data.inputs[0].channelBuffers32[channel];
+				float* outputChannel = data.outputs[0].channelBuffers32[channel];
+
+				int32 tempBufferPos = mBufferPos;
+				for (int32 sample = 0; sample < data.numSamples; sample++)
+				{
+					float tempSample = inputChannel[sample];
+					outputChannel[sample] = mBuffer[channel][tempBufferPos];
+					mBuffer[channel][tempBufferPos] = tempSample;
+					tempBufferPos++;
+					if (tempBufferPos >= delayInSamples)
+						tempBufferPos = 0;
+				}
+			}
+			mBufferPos += data.numSamples;
+			while (delayInSamples && mBufferPos >= delayInSamples)
+				mBufferPos -= delayInSamples;
+		}
+		return kResultTrue;
+		*/
+		return kResultTrue;
 	}
 
 
-	tresult BypassProcessor::setState(IBStream* state)
+	tresult PLUGIN_API BypassProcessor::setState(IBStream* state)
 	{
-		return 0;
+		/*
+		if (!state)
+			return kResultFalse;
+
+		// called when we load a preset, the model has to be reloaded
+
+		float savedDelay = 0.f;
+		if (state->read(&savedDelay, sizeof(float)) != kResultOk)
+		{
+			return kResultFalse;
+		}
+
+		int32 savedBypass = 0;
+		if (state->read(&savedBypass, sizeof(int32)) != kResultOk)
+		{
+			// could be an old version, continue 
+		}
+
+#if BYTEORDER == kBigEndian
+		SWAP_32(savedDelay)
+			SWAP_32(savedBypass)
+#endif
+
+			mDelay = savedDelay;
+		mBypass = savedBypass > 0;
+
+		return kResultOk;
+		*/
+		return kResultTrue;
 	}
 
 
-	tresult BypassProcessor::getState(IBStream* state)
+	tresult PLUGIN_API BypassProcessor::getState(IBStream* state)
 	{
-		return 0;
+		/*
+		// here we need to save the model
+
+		float toSaveDelay = mDelay;
+		int32 toSaveBypass = mBypass ? 1 : 0;
+
+#if BYTEORDER == kBigEndian
+		SWAP_32(toSaveDelay)
+			SWAP_32(toSaveBypass)
+#endif
+
+			state->write(&toSaveDelay, sizeof(float));
+		state->write(&toSaveBypass, sizeof(int32));
+
+		return kResultOk;
+		*/
+		return kResultTrue;
 	}
+
+
 } // namespace Vst
 } // nmamespace Steinberg
--- TheBypasser/TheBypasser/TheBypasser.h
+++ TheBypasser/TheBypasser/TheBypasser.h
@@ -1,10 +1,39 @@
 #pragma once
 
+#include "pluginterfaces/base/fplatform.h"
+
 
 namespace Steinberg {
 namespace Vst {
 
-static const FUID BypassProcessorUID(0x0CDBB668, 0x85D548A9, 0xBFD83719, 0x09D24BB3);
+#if PLATFORM_X64
+#if NDEBUG
+static const FUID BypassProcessorUID(0x5278A87C, 0xEF1911E7, 0xB35DFF3C, 0x61172F1A);
+#else
+static const FUID BypassProcessorUID(0x84F2634C, 0xEF1911E7, 0x8FA2843D, 0x61172F1A);
+#endif
+#else
+#if NDEBUG
+static const FUID BypassProcessorUID(0x9C15CAF0, 0xEF1911E7, 0xAF6F943D, 0x61172F1A);
+#else
+static const FUID BypassProcessorUID(0xAFF7A84A, 0xEF1911E7, 0xA129D83D, 0x61172F1A);
+#endif
+#endif
 
 } // Vst
 } // Steinberg
+
+
+#if PLATFORM_64
+#define PLATFORM_STR "x64"
+#else
+#define PLATFORM_STR "x86"
+#endif
+
+#if NDEBUG
+#define CONFIG_STR "Rel"
+#else
+#define CONFIG_STR "Dbg"
+#endif
+
+#define NAME_STR "The Bypasser " PLATFORM_STR " " CONFIG_STR
